bed303faf625b1027c3dce584a772e6659eb6bd1
Mubarak <desinamubarak123@gmail.com>
2024-07-04 03:04:28 +0100

    update: create firebase config

---
+++ src/firebase/firebase-admin.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import * as admin from 'firebase-admin';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [ConfigModule],
+  providers: [
+    {
+      provide: 'FIREBASE_ADMIN',
+      useFactory: (configService: ConfigService) => {
+        return admin.initializeApp({
+          credential: admin.credential.cert({
+            projectId: configService.get<string>('FIREBASE_PROJECT_ID'),
+            privateKey: configService.get<string>('FIREBASE_PRIVATE_KEY').replace(/\\n/g, '\n'),
+            clientEmail: configService.get<string>('FIREBASE_CLIENT_EMAIL'),
+          }),
+        });
+      },
+      inject: [ConfigService],
+    },
+  ],
+  exports: ['FIREBASE_ADMIN'],
+})
+export class FirebaseAdminModule {}
