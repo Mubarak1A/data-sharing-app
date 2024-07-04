ee88ba729e9e4c3ebf778f65bde5287b01e343a5
Mubarak <desinamubarak123@gmail.com>
2024-07-04 03:24:56 +0100

    update: update auth service functions

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,22 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as admin from 'firebase-admin';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly jwtService: JwtService,
+    @Inject('FIREBASE_ADMIN') private readonly firebaseAdmin: admin.app.App,
+  ) {}
+
+  async validateUser(email: string, password: string): Promise<any> {
+    // Validate user with Firebase
+  }
+
+  async login(user: any) {
+    const payload = { email: user.email, sub: user.uid };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+}
