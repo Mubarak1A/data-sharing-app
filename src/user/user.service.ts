45af368fa4b6ce2d580991bdb6c92b87090f03d5
Mubarak <desinamubarak123@gmail.com>
2024-07-04 08:35:51 +0100

    update: update user service to handle user role

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,14 +1,17 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
+import { UserData } from '../database/entities/user-data.entity';
+import { Image } from '../database/entities/image.entity';
 import { CreateUserDto } from './dto/create-user.dto';
-import { User, UserRole } from './entities/user.entity';
 
 @Injectable()
 export class UserService {
   constructor(
-    @InjectRepository(User)
-    private usersRepository: Repository<User>,
+    @InjectRepository(User) private usersRepository: Repository<User>,
+    @InjectRepository(UserData) private userDataRepository: Repository<UserData>,
+    @InjectRepository(Image) private imageRepository: Repository<Image>,
   ) {}
 
   async createUser(createUserDto: CreateUserDto): Promise<User> {
@@ -18,5 +21,25 @@ export class UserService {
 
   async findByEmail(email: string): Promise<User | undefined> {
     return this.usersRepository.findOne({ where: { email } });
+  }
+
+  async getUserData(userId: number): Promise<UserData[]> {
+    return this.userDataRepository.find({ where: { user: { id: userId } } });
+  }
+
+  async createUserData(userId: number, userData: Partial<UserData>): Promise<UserData> {
+    const user = await this.usersRepository.findOne({ where: { id: userId } });
+    const newUserData = this.userDataRepository.create({ ...userData, user });
+    return this.userDataRepository.save(newUserData);
+  }
+
+  async uploadImage(userId: number, imageUrl: string): Promise<Image> {
+    const user = await this.usersRepository.findOne({ where: { id: userId } });
+    const newImage = this.imageRepository.create({ imageUrl, user });
+    return this.imageRepository.save(newImage);
+  }
+
+  async getUserImages(userId: number): Promise<Image[]> {
+    return this.imageRepository.find({ where: { user: { id: userId } } });
   }
 }
