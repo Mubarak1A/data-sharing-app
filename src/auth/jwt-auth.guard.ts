e90e72501ff09eecd89393f1626a5d28ab0e6f79
Mubarak <desinamubarak123@gmail.com>
2024-07-04 08:58:19 +0100

    create: create jwt auth guard

---
+++ src/auth/jwt-auth.guard.ts
@@ -0,0 +1,28 @@
+// src/auth/jwt-auth.guard.ts
+import { Injectable, ExecutionContext, UnauthorizedException } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Reflector } from '@nestjs/core';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  constructor(private readonly reflector: Reflector, private readonly jwtService: JwtService) {
+    super();
+  }
+
+  canActivate(context: ExecutionContext) {
+    const request = context.switchToHttp().getRequest();
+    const authHeader = request.headers.authorization;
+    if (!authHeader) {
+      throw new UnauthorizedException('Authorization header is missing');
+    }
+    const token = authHeader.split(' ')[1];
+    try {
+      const decodedToken = this.jwtService.verify(token);
+      request.user = decodedToken;
+      return true;
+    } catch (error) {
+      throw new UnauthorizedException('Invalid token');
+    }
+  }
+}
