0f9ed155dd9b2a29b33a0fc2c4b00fed6b337f66
Mubarak <desinamubarak123@gmail.com>
2024-07-04 08:21:39 +0100

    add role prop to user dto

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,5 +1,7 @@
+import { UserRole } from '../entities/user.entity';
+
 export class CreateUserDto {
-    email: string;
-    password: string;
-  }
-  
+  email: string;
+  password: string;
+  role: UserRole;
+}
