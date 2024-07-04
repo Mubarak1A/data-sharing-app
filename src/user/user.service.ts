05749c3fd55dc74d27da803460b72a2a5128d9c6
Mubarak <desinamubarak123@gmail.com>
2024-07-04 08:25:06 +0100

    update: update user service to handle user role

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,17 +1,14 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { User } from './entities/user.entity';
-import { UserData } from '../database/entities/user-data.entity';
-import { Image } from '../database/entities/image.entity';
 import { CreateUserDto } from './dto/create-user.dto';
+import { User, UserRole } from './entities/user.entity';
 
 @Injectable()
 export class UserService {
   constructor(
-    @InjectRepository(User) private usersRepository: Repository<User>,
-    @InjectRepository(UserData) private userDataRepository: Repository<UserData>,
-    @InjectRepository(Image) private imageRepository: Repository<Image>,
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
   ) {}
 
   async createUser(createUserDto: CreateUserDto): Promise<User> {
@@ -19,23 +16,7 @@ export class UserService {
     return this.usersRepository.save(user);
   }
 
-  async getUserData(userId: number): Promise<UserData[]> {
-    return this.userDataRepository.find({ where: { user: { id: userId } } });
-  }
-
-  async createUserData(userId: number, userData: Partial<UserData>): Promise<UserData> {
-    const user = await this.usersRepository.findOne({ where: { id: userId } });
-    const newUserData = this.userDataRepository.create({ ...userData, user });
-    return this.userDataRepository.save(newUserData);
-  }
-
-  async uploadImage(userId: number, imageUrl: string): Promise<Image> {
-    const user = await this.usersRepository.findOne({ where: { id: userId } });
-    const newImage = this.imageRepository.create({ imageUrl, user });
-    return this.imageRepository.save(newImage);
-  }
-
-  async getUserImages(userId: number): Promise<Image[]> {
-    return this.imageRepository.find({ where: { user: { id: userId } } });
+  async findByEmail(email: string): Promise<User | undefined> {
+    return this.usersRepository.findOne({ where: { email } });
   }
 }
